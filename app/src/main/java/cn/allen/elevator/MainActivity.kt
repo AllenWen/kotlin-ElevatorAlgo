02badc8b45887c927ae63cb531c4914de8cc724b
AllenWen <wenxueguo@medlinker.com>
2017-12-06 19:31:36 +0800

    create mainactivity

---
+++ app/src/main/java/cn/allen/elevator/MainActivity.kt
@@ -0,0 +1,17 @@
+package cn.allen.elevator
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Author: AllenWen
+ * CreateTime: 2017/12/6
+ * Email: wenxueguo@medlinker.com
+ * Description:
+ */
+
+class MainActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
